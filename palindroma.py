51dfe9bd4cda02360ca303317a1108748726723e
Rafa <mirandarafa010@gmail.com>
2024-09-04 21:21:59 -0600

    Ejercicio Palindroma

---
+++ palindroma.py
@@ -0,0 +1,14 @@
+def es_palindroma(palabra):
+    # Convertir la palabra a minúsculas y eliminar espacios
+    palabra = palabra.lower().replace(" ", "")
+    # Comprobar si la palabra es igual a su versión invertida
+    return palabra == palabra[::-1]
+
+# Solicitar al usuario que ingrese una palabra
+palabra_usuario = input("Ingresa una palabra: ")
+
+# Verificar si es palíndroma y mostrar el resultado
+if es_palindroma(palabra_usuario):
+    print(f"La palabra '{palabra_usuario}' es palíndroma.")
+else:
+    print(f"La palabra '{palabra_usuario}' no es palíndroma.")
